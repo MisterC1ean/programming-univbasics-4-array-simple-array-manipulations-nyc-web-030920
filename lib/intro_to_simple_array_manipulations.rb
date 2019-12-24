6216bade4070dae0c95d180f6caefbefec79791f
Robert Keller <robert_keller44@icloud.com>
2019-12-24 01:49:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -41,7 +41,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-using_delete_at(array, integer)
-  array.delete(integer)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
 
